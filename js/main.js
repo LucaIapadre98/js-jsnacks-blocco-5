205f7393d0078907cc7315f0e0d7c1854f38d079
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-22 16:10:20 +0200

    snack 3

--- js/main.js
+++ js/main.js
@@ -20,6 +20,18 @@ console.log(people.name);
   // Stampa in console tutti i nomi
   // Risultato: 'Paolo', 'Giulia', 'Marco'
 
+  // #SNACK3 //
+  const numbers = [2, 8, 4, 7, 2, 87];
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+// Risultato: [3, 9, 5, 8, 3, 88]
+
+    for(let i = 0; i < numbers.length; i ++){
+        numbers[i] = numbers[i] + 1;
+    }
+
+console.log(numbers);
+
 
 
 
