c3859142684208368db0c5d5464d1ec9bc8418e8
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-22 16:35:26 +0200

    snack 4

--- js/main.js
+++ js/main.js
@@ -33,6 +33,30 @@ console.log(people.name);
 console.log(numbers);
 
 
+// #SNACK4 //
+const posts = [
+    { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
+    { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
+    { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
+    { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
+  ]
+  
+  // Crea un nuovo array coi nomi di tutti gli autori
+  // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+  for (let i = 0; i < posts.length; i++){
+    console.log(posts[i].author);
+
+  }
+
+
+
+
+
+
+ 
+  
+
 
 
 
