3c84602ef06d0c9d49628e3819ae2ea94ae5678d
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-22 16:40:01 +0200

    snack 5

--- js/main.js
+++ js/main.js
@@ -49,6 +49,24 @@ const posts = [
 
   }
 
+  // #SNACK5 //
+  const nums = [2, 8, 4, 7, 12, 87];
+
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Risultato: [2, 8, 4, 12]
+
+let evenNums = isEven;
+function isEven() {
+    for (let i = 0; i < nums.length; i++) {
+        currentNumber = nums[i];
+        if (nums[i] % 2 == 0){
+            console.log(nums[i]);
+        }
+    }
+}
+console.log(isEven());
+
+
 
 
 
