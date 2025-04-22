c7fa135b92549a47f1d653f94706312bdbec1b79
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-22 15:54:10 +0200

    snack 1-2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+// #SNACK 1 //
+//const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//console.log(`${names}`);
+
+
+// #SNACK 2 //
+const people = [
+    { name: 'Paolo', age: 35 },
+    { name: 'Giulia', age: 24 },
+    { name: 'Marco', age: 67 }
+  ];    
+
+console.log(people.name);
+
+  
+  // Stampa in console tutti i nomi
+  // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+
+
+
+
+  
